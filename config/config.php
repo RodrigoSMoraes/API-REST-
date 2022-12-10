bfb9e7a9aa15bab83f594ffa664dc9137cd27ff9
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2022-12-10 00:08:58 -0300

    feat: configurações iniciais

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php 
+define("ROOT_PATH", __DIR__ . "/../");
+
+define("DATABASE_FILE", ROOT_PATH . 'database.json');
+
+require_once ROOT_PATH . "/controller/api/baseController.php";
+
+require_once ROOT_PATH . "/model/userModel.php";
+
+
+
+
+?>
