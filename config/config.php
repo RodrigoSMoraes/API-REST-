e0e3b6da2160b4adbef31258b23dfbab22494207
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2022-12-13 11:01:07 -0300

    feat: index

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define("DATABASE_FILE", ROOT_PATH . 'database.json');
 
 
 
-require_once ROOT_PATH . "/controller/api/baseController.php";
+require_once ROOT_PATH . "/controller/API/baseController.php";
 
 require_once ROOT_PATH . "/model/userModel.php";
 
--- index.php
+++ index.php
@@ -2,5 +2,23 @@
 require __DIR__ . "/config/config.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+
+if ((isset($uri[1]) && $uri[1] != 'api') || (isset($uri[2]) && $uri[2] != 'v1')) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+} else if ((isset($uri[3]) && $uri[3] != 'user') || !isset($uri[4])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+    
+}
+
+
+require ROOT_PATH . "/controller/API/userController.php";
+
+$user = new userController();
+
+$methodName = $uri[4] . 'action';
+$user->{methodName}();
 
 ?>
